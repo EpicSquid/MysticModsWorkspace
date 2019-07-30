3f1caf7c5f0ddff0682abc0fc6df605121eb8f01
EpicSquid <epicsquid315@gmail.com>
2019-07-31 09:23:22 +1000

    Created the files for a MultiMod workspace
    Thx Mcjty!

---
+++ setupMysticWorkspace.bat
@@ -0,0 +1,19 @@
+@echo Cloning repos
+
+git clone https://github.com/MysticMods/MysticalLib.git
+git clone https://github.com/MysticMods/MysticalWorld.git
+git clone https://github.com/MysticMods/Embers.git
+git clone https://github.com/MysticMods/Traverse.git
+
+@echo Switching to 1.14.4 branches
+cd MysticalLib
+git checkout 1.14
+cd ../
+cd MysticalWorld
+git checkout 1.14
+cd ../
+cd Embers
+git checkout 1.14
+cd ../
+
+@echo Done!
