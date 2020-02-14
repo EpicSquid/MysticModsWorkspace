2562993e6b31fc4facea9eae6a0569bffe6e0365
Jon McManus <due@wxwhatever.com>
2020-02-14 11:14:04 +1000

    Simplified workspace with updated gradle.

--- setupMysticWorkspace.bat
+++ setupMysticWorkspace.bat
@@ -2,37 +2,13 @@
 
 git clone https://github.com/MysticMods/MysticalLib.git
 git clone https://github.com/MysticMods/MysticalWorld.git
-git clone https://github.com/MysticMods/Embers.git
-git clone https://github.com/MysticMods/Traverse.git
-git clone https://github.com/MysticMods/Firefly.git
-@echo skipping git clone https://github.com/MysticMods/Glimmering.git
-git clone https://github.com/duely/dwmh.git
-git clone https://github.com/AranaiRa/ArcaneArchives.git
-git clone https://github.com/MysticMods/GadgetryCore.git
 git clone https://github.com/MysticMods/MysticalMachinery.git
-git clone https://github.com/MysticMods/EmsFarming.git
-git clone https://github.com/MysticMods/Roots.git
 
 @echo Switching to 1.14.4 branches
 cd MysticalLib
 git checkout 1.14
 cd ../
 cd MysticalWorld
-git checkout 1.14
-cd ../
-cd Embers
-git checkout 1.14
-cd ../
-cd dwmh
-git checkout 1.14
-cd ..
-cd ArcaneArchives
-git checkout 1.14
-cd ..
-cd GadgetryCore
-git checkout 1.14
-cd ../
-cd Roots
 git checkout 1.14
 cd ../
 
